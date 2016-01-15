194b3b7bff94e5f3599835563d810837211d2b2a
Christoph Klein <christoph.t.klein@me.com>
2016-01-15 11:09:57 -0600

    Simple full workflow for ethane box is working

--- demos/ethane_box.py
+++ demos/ethane_box.py
@@ -1,11 +1,12 @@
-import itertools as it
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import mbuild as mb
 import metamds as mds
 import mdtraj as md
 
 
-def build_system(box, n_molecules, **kwargs):
+def build_ethane_box(box, n_molecules, **kwargs):
     from mbuild.examples import Ethane
     ethane = Ethane()
     full_box = mb.fill_box(ethane, n_molecules, box)
@@ -13,7 +14,19 @@ def build_system(box, n_molecules, **kwargs):
     return full_box
 
 
-def build_script(compound, forcefield, **kwargs):
+# Input parameters
+parameters = {'n_molecules': 200,
+              'box': [3, 3, 3],
+              'forcefield': 'OPLS-aa'}
+
+# Build the initial configuration
+compound = build_ethane_box(**parameters)
+compound.visualize()
+
+parameters['compound'] = compound
+
+
+def create_run_script(compound, forcefield, **kwargs):
     name = compound.name
     em = 'em.mdp'
     nvt = 'nvt.mdp'
@@ -22,49 +35,41 @@ def build_script(compound, forcefield, **kwargs):
 
     compound.save(top, forcefield=forcefield, overwrite=True)
 
-    em_grompp = 'gmx grompp -f {mdp} -c {gro} -p {top} -o em.tpr'.format(
-        mdp=em, gro=gro, top=top)
+    em_grompp = 'gmx grompp -f {mdp} -c {gro} -p {top} -o em.tpr'.format(mdp=em, gro=gro, top=top)
     em_mdrun = 'gmx mdrun -v -deffnm em'
 
-    nvt_grompp = 'gmx grompp -f {mdp} -c em.gro -p {top} -o nvt.tpr'.format(
-        mdp=nvt, top=top)
+    nvt_grompp = 'gmx grompp -f {mdp} -c em.gro -p {top} -o nvt.tpr'.format(mdp=nvt, top=top)
     nvt_mdrun = 'gmx mdrun -v -deffnm nvt'
 
     script = (em_grompp, em_mdrun, nvt_grompp, nvt_mdrun)
     return script
 
-
 if __name__ == '__main__':
-    sim = mds.Simulation(name='ethane', template=build_script, project_dir='ethane_box')
+    # Initialize a simulation instance with a template and some metadata
+    sim = mds.Simulation(name='ethane', template=create_run_script, project_dir='ethane_box')
 
-    # Input parameters
-    parameters = {'n_molecules': 1,
-                  'box': [3, 3, 3],
-                  'forcefield': 'OPLS-aa'}
-
-    # Build
-    compound = build_system(**parameters)
-    parameters['compound'] = compound
-
+    # Parameterize our simulation template
     task = sim.parametrize(**parameters)
 
     # Run
-    job = task.execute(remote='', credentials='')
+    task.execute(remote='', credentials='')
+    # task.status()
 
-    exit()
-    import ipdb; ipdb.set_trace()
     # Analyze
-    job.status()
+    trajectories = task.get_output('trajectories')
+    topologies = task.get_output('topologies')
+    # Pick which one to select?
 
-    output_traj = job.output['trajectories']
-    # job.output['logs']
-    traj = md.load(output_traj, top='')
+    traj = md.load('nvt.xtc', top='nvt.gro')
 
-
-
-
-
-
-
-
-
+    # RDF
+    # pairs = traj.top.select_pairs('name C', 'name C')
+    # r, g_r = md.compute_rdf(traj, pairs)
+    # plt.plot(r, g_r)
+    # plt.xlabel('r (nm)')
+    # plt.ylabel('g(r)')
+    #
+    # s2 = md.compute_nematic_order(traj, 'residues')
+    # plt.plot(traj.time, s2)
+    # plt.xlabel('time (ps)')
+    # plt.ylabel('S2')
