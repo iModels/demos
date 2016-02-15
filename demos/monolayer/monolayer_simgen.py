e5fc2996deb50f0c409e5508f35d6a2119ddfe95
sallai <sallai.janos@vanderbilt.edu>
2016-02-15 03:38:21 -0600

    added monolayer blockly code, did some restructuring

--- demos/monolayer/monolayer_simgen.py
+++ demos/monolayer/monolayer_simgen.py
@@ -1,5 +1,6 @@
-import time
 import os
+import logging
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,68 +11,77 @@ import metamds as mds
 import mdtraj as md
 
 
+from simgen.project import Project
+
+OFFLINE = True
+
 def build_monolayer(chain_length, n_molecules, **kwargs):
     from mbuild.examples import AlkaneMonolayer
     pattern = mb.Random2DPattern(n_molecules)
     monolayer = AlkaneMonolayer(pattern, tile_x=1, tile_y=1, chain_length=chain_length)
     monolayer.name = 'alkane_n-{}_l-{}'.format(n_molecules, chain_length)
     mb.translate(monolayer, [0, 0, 2])
+    box = monolayer.boundingbox
+    monolayer.periodicity += np.array([0, 0, 5 * box.lengths[2]])
+
     return monolayer
 
+def generate_code(**parameters):
+    # import pdb; pdb.set_trace()
 
-def create_run_script(build_func, forcefield, input_dir, **kwargs):
-    compound = build_func(**kwargs)
-    name = compound.name
-    em = os.path.join(input_dir, 'em.mdp')
-    nvt = os.path.join(input_dir, 'nvt.mdp')
-    gro = '{name}.gro'.format(name=name)
-    top = '{name}.top'.format(name=name)
+    if OFFLINE:
+        # load simgen files from local folders
+        res_dir = os.path.join(os.path.dirname(__file__), 'simgen_project')
 
-    box = compound.boundingbox
-    compound.periodicity += np.array([0, 0, 5 * box.lengths[2]])
-    compound.save(top, forcefield=forcefield, overwrite=True)
+        manifest = {
+            'title': 'Monolayer example',
+            'code_path': [os.path.join(res_dir, 'code')],
+            'concept_path': [os.path.join(res_dir, 'concepts')],
+            'template_path': [os.path.join(res_dir, 'templates')]
+        }
 
-    em_grompp = 'gmx grompp -f {mdp} -c {gro} -p {top} -o em.tpr'.format(mdp=em, gro=gro, top=top)
-    em_mdrun = 'gmx mdrun -v -deffnm em -ntmpi 1'
+        project = Project(manifest)
+    else:
+        # load sigmen files from GitHub
+        project = Project(os.path.join(os.path.dirname(__file__), 'simgen_project', 'online_project.yaml'))
 
-    nvt_grompp = 'gmx grompp -f {mdp} -c em.gro -p {top} -o nvt.tpr'.format(mdp=nvt, top=top)
-    nvt_mdrun = 'gmx mdrun -v -deffnm nvt -ntmpi 1'
-
-    script = (em_grompp, em_mdrun, nvt_grompp, nvt_mdrun)
-    return script
+    return project.render_tasks('prg', output_dir='./', inject_dict=parameters)
 
 if __name__ == '__main__':
+    # # configure logging
+    # logging.basicConfig(format='%(module)s:%(lineno)d %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=0)
+
     # Input parameters
     parameters = {'chain_length': 10,
                   'n_molecules': 100,
-                  'forcefield': 'OPLS-aa'}
+                  'forcefield': 'OPLS-aa',
+                  }
 
     # Build the initial configuration
     compound = build_monolayer(**parameters)
+    parameters['compound'] = compound
+    parameters['system_name'] = compound.name
+
     #compound.visualize()
 
-    parameters['build_func'] = build_monolayer
-
     # Initialize a simulation instance with a template and some metadata
-    sim = mds.Simulation(name='monolayer', template=create_run_script, input_dir='static_input_files', output_dir='output')
+    sim = mds.Simulation(name='monolayer', template=generate_code, input_dir='static_input_files', output_dir='simgen_output')
 
     # Parameterize our simulation template
     task = sim.parametrize(**parameters)
 
     # Run
     task.execute()
-    exit()
-    task.execute(hostname='rahman.vuse.vanderbilt.edu', username='ctk3b')
-    print(task.status())
+    # task.execute(hostname='rahman.vuse.vanderbilt.edu', username='ctk3b')
+    # print(task.status())
 
-    time.sleep(10)
-    task.sync()
+    # time.sleep(10)
+    # task.sync()
 
     # Analyze
     trajectories = task.get_output_files('trajectories')
     topologies = task.get_output_files('topologies')
     # Pick which one to select?
-    import pdb; pdb.set_trace()
 
     trj_path = os.path.join(task.output_dir, 'nvt.xtc')
     top_path = os.path.join(task.output_dir, 'em.gro')
